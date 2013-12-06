d9f6a4f5a71bfbced778ea59f58ed58671644aa4
zaeemul <zaeemul.haque@gmail.com>
2013-12-05 23:12:58 -0500

    with image---all final

--- _server/web_unformatting.php
+++ _server/web_unformatting.php
@@ -1,15 +1,26 @@
 <?php
-	$string = $_POST['str'];			
+    // ----- remove HTML TAGs ----- 
+	$string = $_POST['str'];			//get the text
 
+	//function to format text
 	function removeFormatting($text)
 	{
-		
+		//remove tags
 		$text = preg_replace ('/<[^>]*>/', '', $text);
+		//remove footnotes/refrences
 		$text = preg_replace ('#\[\d+\]#', '', $text);
+		//remove multiple empty lines/tabs/
 		$text = preg_replace ('/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/', "", $text);
 
-		$text = str_replace("\t", ' ', $text);   
-		$text = str_replace("\t", ' ', $text);   
+		// ----- remove control characters ----- 
+		//$string = str_replace("\n", ' ', $string);    // --- replace with empty space
+		$text = str_replace("\t", ' ', $text);   // --- replace with space
+		$text = str_replace("\t", ' ', $text);   // --- replace with space
+		// ----- remove multiple spaces -----     
+		$text = trim(preg_replace('/ {2,}/', ' ', $text));
+		//remove slashes
+		$text = str_replace("\\", '', $text);   // --- replace with space
+		return $text;
 	}
 	echo removeFormatting($string);
 
